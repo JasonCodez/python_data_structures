c4de0ad00293ee20aafedc74606eecf51e418ade
JasonCodez <jason13garrett@gmail.com>
2021-05-23 02:13:12 -0500

    added python data structures exercises

---
+++ 07_multiple_letter_count/multiple_letter_count.py
@@ -0,0 +1,14 @@
+def multiple_letter_count(phrase):
+    """Return dict of {ltr: frequency} from phrase.
+
+        >>> multiple_letter_count('yay')
+        {'y': 2, 'a': 1}
+
+        >>> multiple_letter_count('Yay')
+        {'Y': 1, 'a': 1, 'y': 1}
+    """
+    newdict = {}
+    for char in phrase:
+       newdict[char] = phrase.count(char)
+    return newdict
+
